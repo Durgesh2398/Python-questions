92c12c016926fc89acde3cd486adcff0d97c74fe
Durgesh2398 <2308durgesh@gmail.com>
2019-11-29 00:20:18 +0530

    2st day

--- pattern.py
+++ pattern.py
@@ -23,3 +23,68 @@ for a in range(size, -1, -1):
     for b in range(1, (a * 2)): 
         print("*", end = "") 
     print("")
+
+#-------------------------------------------------
+# Program to print full pyramid
+num_rows = int(input("Enter the number of rows"))
+for i in range(0, num_rows):
+    for j in range(0, num_rows-i-1):
+        print(end=" ")
+    for j in  range(0, i+1):
+        print("*", end=" ")
+    print()
+
+#------------------------------------------------
+    #Program to print Left Half Pyramid
+num_rows = int(input("Enter the number of rows"))
+k = 1
+for i in range(0, num_rows):
+    for j in range(0, k):
+        print("* ", end="")
+    k = k + 1
+    print()
+
+#------------------------------------------------
+#Program to print Right Half Pyramid
+num_rows = int(input("Enter the number of rows"))
+k = 8
+for i in range(0, num_rows):
+    for j in range(0, k):
+        print(end=" ")
+        k = k - 2
+for j in range(0, i+1):
+    print("* ", end="")
+print()
+
+#---------------------------------------------
+# Program to print One More Star Pattern Pyramid
+print("Program to print star pattern: \n")
+rows = input("Enter maximum stars you want display on a single line")
+rows = int (rows)
+for i in range (0, rows):
+    for j in range(0, i + 1):
+        print("* ", end='')
+    print("\r")
+    for i in range (rows, 0, -1):
+        for j in range(0, i -1):
+            print("* ", end='')
+        print("\r")
+
+
+#---------------------------------------------
+print("Program to print star pattern in different style: \n")
+num_rows = int(input('Please enter the number of rows'))
+for i in range (0,num_rows):
+    for j in range (num_rows,i,-1):
+        print("* ", end="")
+    print()
+
+
+#---------------------------------------------
+num_rows = int(input("Please enter the number of rows"))
+for i in range(num_rows,0,-1):
+    for j in range(0, num_rows-i):
+        print(end=" ")
+for j in range(0,i):
+    print("* ", end=" ")
+print()
