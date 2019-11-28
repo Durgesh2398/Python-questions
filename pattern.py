4a53a089b17bcf5878ba609ce4134df16e378f78
Durgesh2398 <2308durgesh@gmail.com>
2019-11-28 20:54:11 +0530

    1st day

--- pattern.py
+++ pattern.py
@@ -5,3 +5,21 @@ for row in range(6):
         else:
             print(end=" ")
     print()
+
+size = 15
+for a in range(int(size / 2), size + 1, 2): 
+    for b in range(1, size - a, 2):  
+        print(" ", end = "") 
+    for b in range(1, a + 1): 
+        print("*",end="") 
+    for b in range(1, (size - a) + 1): 
+        print(" ", end = "") 
+    for b in range(1, a): 
+        print("*", end = "") 
+    print("") 
+for a in range(size, -1, -1): 
+    for b in range(a, size): 
+        print(" ", end = "") 
+    for b in range(1, (a * 2)): 
+        print("*", end = "") 
+    print("")
