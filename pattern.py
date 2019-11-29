d585b6a2008bbeb5c5881b5dea39f1cd67d2099f
Durgesh2398 <2308durgesh@gmail.com>
2019-11-29 17:48:42 +0530

    2nd day

--- pattern.py
+++ pattern.py
@@ -88,3 +88,13 @@ for i in range(num_rows,0,-1):
 for j in range(0,i):
     print("* ", end=" ")
 print()
+
+
+#-----------------------------------------------
+for row in range(7):
+    for col in range(5):
+        if ((col==0 or col ==4) and row!=0) or ((row==0 or row==3) and (col>0 and col<4)):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
